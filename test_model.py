02b51f748cb5f6123cb73e0065fd2b1997842812
shauncss <shauncss@hotmail.com>
2026-02-23 01:10:42 +0800

    123

--- test_model.py
+++ test_model.py
@@ -1,15 +1,15 @@
 from ultralytics import YOLO
-import os
 
+# The "Shield" for Windows multiprocessing
 if __name__ == '__main__':
+    
     # 1. Load the model
-    model = YOLO('yolo11m.pt') 
+    model = YOLO('yolo11n.pt') 
 
     # 2. Train the model
     model.train(
         data="construction-ppe.yaml", 
-        epochs=50, 
-        imgsz=1280, 
-        batch=4, 
+        epochs=10, 
+        imgsz=640,
         device=0
-    )
+    )   
